9490dec03f6843572780757d4eb40bc7f798f801
RyzenMu <muruga13in2022@gmail.com>
2025-01-10 06:12:13 +0530

    header section completed

--- 2/charity/src/App.js
+++ 2/charity/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Header from "./header";
+import Main from "./main";
 function App() {
-  return <Header className="header" />;
+  return (
+    <>
+      <Header className="header" />
+      <Main />
+    </>
+  );
 }
 
 export default App;
