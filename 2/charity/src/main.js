0da37bce6a38c6ec35110fe883a9685a6e42c69f
RyzenMu <muruga13in2022@gmail.com>
2025-01-10 06:12:13 +0530

    header section completed

---
+++ 2/charity/src/main.js
@@ -0,0 +1,17 @@
+function Main() {
+  return (
+    <div>
+      <section className="section-1">
+        <p className="section-1-text">Our Helping to the World &emsp;</p>
+      </section>
+      <section className="section-2">Section 2</section>
+      <section className="section-3">Section 3</section>
+      <section className="section-4">Section 4</section>
+      <section className="section-5">Section 5</section>
+      <section className="section-6">Section 6</section>
+      <section className="section-7">Section 7</section>
+    </div>
+  );
+}
+
+export default Main;
