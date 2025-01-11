339bedd7c80f758d159d434a7200ded58119975d
RyzenMu <muruga13in2022@gmail.com>
2025-01-11 07:42:44 +0530

    charity website completed

--- 2/charity/src/main.js
+++ 2/charity/src/main.js
@@ -1,10 +1,44 @@
+import Card from "./section-2.cards";
+import icon1 from "./img/section-2-icon-1.svg";
+import icon2 from "./img/section-2-icon-2.svg";
+import icon3 from "./img/section-2-icon-3.svg";
+
 function Main() {
   return (
     <div>
       <section className="section-1">
         <p className="section-1-text">Our Helping to the World &emsp;</p>
+        <button
+          style={{
+            border: "none",
+            backgroundColor: "rgba(0,255,0, 0.345)",
+            marginRight: "10%",
+          }}
+        >
+          Donate
+        </button>
       </section>
-      <section className="section-2">Section 2</section>
+      <section className="section-2">
+        <div className="section-2-title">
+          <p>What we are Doing</p>
+          <h3>We are in a mission to help helpless</h3>
+        </div>
+
+        <div className="section-2-cards">
+          <Card>
+            <img src={icon1} alt="icon1" />
+          </Card>
+
+          <Card>
+            <img src={icon2} alt="icon2" />
+          </Card>
+
+          <Card>
+            {" "}
+            <img src={icon3} alt="icon3" />
+          </Card>
+        </div>
+      </section>
       <section className="section-3">Section 3</section>
       <section className="section-4">Section 4</section>
       <section className="section-5">Section 5</section>
---
+++ 2/charity/src/section-2.cards.js
@@ -0,0 +1,14 @@
+function Card({ children }) {
+  return (
+    <div className="section-2-each-card">
+      <span>{children}</span>
+      <h3>Clean Water</h3>
+      <p>
+        The sea freight service has grown conside rably in recent years. We
+        spend timetting to know your processes to.
+      </p>
+    </div>
+  );
+}
+
+export default Card;
